184092898f5d93cbe4c4bd87ea1872a729806079
KarimLulu <karim@innovizo.com>
2017-08-03 20:43:13 +0300

    added database adapter

--- Name server/db_adapter.py
+++ Name server/db_adapter.py
@@ -2,24 +2,95 @@ import sqlite3
 import sys
 import pickle
 from sqlalchemy import Column, Integer, String, create_engine, exc, orm
+from sqlalchemy.ext.declarative import declarative_base
+from contextlib import contextmanager
 
 
 DBNAME = 'name_server'
+folder = os.path.dirname(os.path.realpath(__file__))
+DSN = 'sqlite:///{path}.db'.format(path=os.path.join(folder, DBNAME))
+
+
+Base = declarative_base()
+
+class Service(Base):
+    __tablename__ = 'services'
+    id = Column('id', Integer, primary_key=True)
+    host = Column('host', String)
+    port = Column('port', Integer)
+    name = Column('name', String)
+    description = Column('description', String)
+    __table_args__ = (UniqueConstraint('host', 'port', name='address_cnt'),)
+
+    def __repr__(self):
+        return "<Service(name='{name}', address='{addr}', description='{desc}')>".format(name=self.name,
+                                                                                         addr=self.address,
+                                                                                         desc=self.description)
+    def __str__(self):
+        return repr(self)
+
+
+    @property
+    def address(self):
+        return '{host}:{port}'.format(host=self.host, port=self.port)
 
 
 class DatabaseAdapter(object):
 
     def __init__(self, dsn):
         try:
-            self.engine = create_engine(dsn)
+            eng = create_engine(dsn)
         except Exception as err:
             raise
 
         try:
-            self.engine.connect()
+            eng.connect()
         except:
             raise
+        self.Session = orm.sessionmaker(bind=eng)
+        self.services = Service.__table__
+        self.eng = self.services.metadata.bind = eng
 
 
+    @contextmanager
+    def session_scope(self):
+        session = self.Session()
+        try:
+            yield session
+            session.commit()
+        except Exception as error:
+            session.rollback()
+            raise
+        finally:
+            session.close()
 
+
+    def upsert(self, obj):
+        with self.session_scope() as ses:
+            query = ses.query(Service).filter(Service.host==obj.host, Service.port==obj.port)
+            if query.count():
+                query.update({Service.name: obj.name,
+                              Service.description: obj.description or Service.description
+                             })
+            else:
+                ses.add(obj)
+
+
+    def get_service_by_name(self, name):
+        with self.session_scope() as ses:
+            query = ses.query(Service).filter(Service.name==name)
+            total = query.count()
+            if total > 1:
+                msg = 'There are several services with the name `{name}`:\n'.format(name=name)
+                for i,service in enumerate(query):
+                    msg += '{idx}. {service}\n'.format(idx=i+1, service=service)
+            elif total == 1:
+                msg = 'Address of the requested service is {addr}'.format(addr=query.one().address)
+            else:
+                msg = 'Sorry, there is no service with the name `{name}`'.format(name=name)
+            return msg
+
+
+if __name__=='__main__':
+    db = DatabaseAdapter(DSN)
 
